c127eed2e43e109fe12f3ef8d62ed473b36ba3b5
Henry Hellbusch <hhellbusch@gmail.com>
2018-06-27 09:58:09 -0500

    implemented credit check (random)

--- routes/api.js
+++ routes/api.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/check', function(req, res, next){
+  let result = Math.random() >= 0.1;
+  let response = {
+    "data" : {
+      "validity" : result
+    }
+  };
+  res.send(response);
+});
+
 module.exports = router;
