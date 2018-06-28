9d56188c99a05e57a92ffc8eaea947b2edbd8888
Henry Hellbusch <hhellbusch@gmail.com>
2018-06-28 11:44:52 -0500

    created end points for override of result from /api/check and /api/payment/process
    to enable and set return as "true":
    /api/payment/process/overrideProcessResult/enable/1
    
    to enable and set return as "false":
    /api/payment/process/overrideProcessResult/enable/0
    
    to disable / enable randomness:
    /api/payment/overrideProcessResult/disable
    
    new response structure for /api/check and /api/payment/process -
    {
      data: {
        validity: true,
        overrideProcessPaymentEnabled: false
      }
    }

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,60 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+let overrideProcessPayment = false;
+let overrideProcessPaymentValue = false;
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  let response = {
+    "_links" : [
+      {
+        "uri" : "/check"
+      }
+    ]
+  };
+  res.send(response);
 });
 
-router.get('/check', function(req, res, next){
+let handlePayment = function() {
   let result = Math.random() >= 0.1;
+  if (overrideProcessPayment) {
+    result = overrideProcessPaymentValue;
+  }
   let response = {
     "data" : {
-      "validity" : result
+      "validity" : result,
+      "overrideProcessPaymentEnabled" : overrideProcessPayment
     }
+  };
+  return response;
+};
+
+// keeping for legacy
+router.get('/check', function(req, res, next){
+  let response = handlePayment();
+  res.send(response);
+});
+
+router.get('/payment/process', function(req, res, next){
+  let response = handlePayment();
+  res.send(response);
+});
+
+// end point to set response
+router.get('/payment/overrideProcessResult/enable/:value', function(req, res, next){
+  overrideProcessPayment = true;
+  overrideProcessPaymentValue = Boolean(Number(req.params.value));
+  let response = {
+    "overrideProcessPayment" : overrideProcessPayment,
+    "overrideProcessPaymentValue" : overrideProcessPaymentValue
+  };
+  res.send(response);
+});
+
+router.get('/payment/overrideProcessResult/disable/', function(req, res, next){
+  overrideProcessPayment = false;
+  let response = {
+    "overrideProcessPayment" : overrideProcessPayment
   };
   res.send(response);
 });
